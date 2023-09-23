21220bd379cef739edab02e1e367f6785f77af2c
javiertzr01 <javiertzr1@gmail.com>
2023-09-24 05:25:47 +0800

    Completed Physics 2D, need to settle Question-Box-Spring edgecollider layer issue

--- Assets/Scripts/Week 1/PlayerMovement.cs
+++ Assets/Scripts/Week 1/PlayerMovement.cs
@@ -71,6 +71,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log(1 << collision.transform.gameObject.layer);
         if (((collisionLayerMask & (1 << collision.transform.gameObject.layer)) > 0) && !onGroundState)
         {
             onGroundState = true;
